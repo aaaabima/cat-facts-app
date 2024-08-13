cc253a2501bb1473079735dbdbe9d51a75d11805
aaaabima <aaaabim@gmail.com>
2024-08-13 19:21:23 +0700

    feat: configure project dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,13 +18,22 @@ android {
     }
 
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
+            buildConfigField("String", "BASE_URL", "\"https://api.chucknorris.io/\"")
+        }
+        release {
+            isMinifyEnabled = true
+            buildConfigField("String", "BASE_URL", "\"https://api.chucknorris.io/\"")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -36,6 +45,28 @@ android {
 }
 
 dependencies {
+    // Retrofit Components
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // Lifecycle Components
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    //  Kotlin Components
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    // Glide Component
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    // Dagger Components
+    implementation("com.google.dagger:dagger:2.44.2")
+    annotationProcessor("com.google.dagger:dagger-compiler:2.44.2")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
